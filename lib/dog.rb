ac6fde99a20b6b07ad1a2b32bb2859aadfc7c1ad
BRIAN BETT <+>
2023-05-19 12:46:28 +0300

    Completed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,10 @@
 class Dog
+    attr_writer :name
+    attr_accessor :breed
+    attr_reader :name
 
+    def initialize
+        @breed = "Beagle"
+        @name = "Fido"
+    end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
+    attr_accessor :name
+    attr_accessor :job
 
+    def initialize
+        @name = "Beyonce"
+        @job = "Singer"
+    end
 end
